943e31431dec2dd68ba03d88b50eb73ab0c6aa77
ordinCode <ysh1346@naver.com>
2020-04-29 22:12:12 +0900

    feat - 게임 시작시 room PathVariable 사용

--- src/main/java/chess/controller/WebController.java
+++ src/main/java/chess/controller/WebController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,8 +39,8 @@ public class WebController {
         return "start";
     }
 
-    @PostMapping("/game")
-    public String game(@RequestParam Integer roomId,
+    @PostMapping("/room/{roomId}/game")
+    public String game(@PathVariable Integer roomId,
         @RequestParam(defaultValue = "WHITE") String whiteName,
         @RequestParam(defaultValue = "BLACK") String blackName,
         Model model) {
